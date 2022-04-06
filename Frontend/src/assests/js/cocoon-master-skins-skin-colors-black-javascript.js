f8b9ba95f5ecc15ec7217fb5fe009e12d4fd91e3
PROCW <developerteam888@outlook.com>
2022-04-06 12:57:06 -0700

    del

--- Frontend/src/assests/js/cocoon-master-skins-skin-colors-black-javascript.js
+++
@@ -1,3 +0,0 @@
-//ここにスキンで利用するJavaScriptを記入する
-
-/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
--- Frontend/src/assests/js/themes-cocoon-child-master-javascript.js
+++
@@ -1,4 +0,0 @@
-//ここに追加したいJavaScript、jQueryを記入してください。
-//このJavaScriptファイルは、親テーマのJavaScriptファイルのあとに呼び出されます。
-//JavaScriptやjQueryで親テーマのjavascript.jsに加えて関数を記入したい時に使用します。
-/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
