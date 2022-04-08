659075b1f3a74ca9683fba2d87bbbb0e35e563d3
PROCW <developerteam888@outlook.com>
2022-04-08 14:24:31 -0700

    update

---
+++ Frontend/src/lngProvider/index.js
@@ -0,0 +1,14 @@
+import enLang from './entries/en-US'
+import jaLang from './entries/ja-JP'
+import idLang from './entries/id-ID'
+import viLang from './entries/vi-VI'
+
+
+const AppLocale = {
+  en: enLang,
+  ja: jaLang,
+  id: idLang,
+  vi: viLang,
+}
+
+export default AppLocale
