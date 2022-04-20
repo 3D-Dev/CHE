77e3d354f5c5772797671a8f5d5f1c8d6b447b52
proactive-dev <proactive.d20@gmail.com>
2022-04-20 13:43:36 -0700

    Add gas options for ERC20 transfer

--- api/app/constant/chainHelper.js
+++ api/app/constant/chainHelper.js
@@ -28,10 +28,14 @@ const contract = new ethers.Contract(process.env.HBY_TOKEN_ADDRESS, contractAbiF
 
 // Transfer ERC20 tokens from admin wallet
 // param: amount, example '1.0'
-const transferHBYFromAdmin = async (toAddress, amount) => {
-  contract.transfer(toAddress, ethers.utils.parseUnits(amount, "ether")).then((result) => {
+const transferHBYFromAdmin = async (toAddress, amount, fee=10) => {
+  let options = {
+    gasPrice: ethers.utils.parseUnits(`${fee}`, "gwei"),
+    gasLimit: ethers.BigNumber.from("95000"),
+  };
+
+  contract.transfer(toAddress, ethers.utils.parseUnits(amount, "ether"), options).then((result) => {
     console.log('###############################transferHBYFromAdmin_result: ',result.hash)
-    return hash
   }).catch((error) => {
     console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@transferHBYFromAdmin_error: ',error)
   })
