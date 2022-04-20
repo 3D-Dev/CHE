4d6bad6abfce58fb13d8171e3ce6036f187580ed
PROCW <developerteam888@outlook.com>
2022-04-20 13:37:36 -0700

    Revert "update"
    This reverts commit 67555c0a323d429af54075a8d9c02a7434290798.

--- api/server.js
+++ api/server.js
@@ -5,9 +5,8 @@ const cron = require('node-cron');
 const requests = require('./app/controllers/requests.controller');
 
 const app = express()
-const whitelist = ['http://192.168.1.114:3000', 'http://192.168.1.114:3001']
 //const whitelist = ['http://localhost:3000', 'http://localhost:3001']
-//const whitelist = ['http://35.78.212.16', 'http://35.78.212.16:3000']
+const whitelist = ['http://35.78.212.16', 'http://35.78.212.16:3000']
 global.__basedir = __dirname
 
 var corsOptions = {
